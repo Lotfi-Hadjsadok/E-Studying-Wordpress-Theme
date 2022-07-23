ab08ecefa60092232b6390f0b1a3906bbfdcd4a1
lotfi-hadjsadok <lotfihadjsadok@gmail.com>
2022-07-23 02:17:38 +0200

    fix user bug

--- templates/search-specialities.php
+++ templates/search-specialities.php
@@ -4,25 +4,29 @@ use Inc\Model\Speciality;
 
 $specialities = new Speciality();
 $specialities = $specialities->get_specialities();
-$visited_specialities = get_user_meta(get_current_user_id(), '_speciality_visits', true) ?: null;
-if ($visited_specialities == null) {
-    foreach ($specialities as $speciality) {
-        // TO PUT THE POST ID AS ARRAY_KEY
-        $specialities_to_push[$speciality->ID] = $speciality;
-        $specialities_to_push[$speciality->ID]->visits_count = 0;
+if (get_current_user_id()) {
+    $visited_specialities = get_user_meta(get_current_user_id(), '_speciality_visits', true) ?: null;
+    if ($visited_specialities == null) {
+        foreach ($specialities as $speciality) {
+            // TO PUT THE POST ID AS ARRAY_KEY
+            $specialities_to_push[$speciality->ID] = $speciality;
+            $specialities_to_push[$speciality->ID]->visits_count = 0;
+        }
+        update_user_meta(get_current_user_id(), '_speciality_visits', $specialities_to_push);
     }
-    update_user_meta(get_current_user_id(), '_speciality_visits', $specialities_to_push);
-}
 
-uasort($visited_specialities, function ($a, $b) {
-    return $a->visited_count < $b->visited_count;
-});
+    uasort($visited_specialities, function ($a, $b) {
+        return $a->visited_count < $b->visited_count;
+    });
+
+    $specialities = $visited_specialities;
+}
 
 ?>
 <div class="e-search">
     <select class="e-search-specialities" name="" id="">
         <option value="" selected disabled>Selectionner votre spécialitié</option>
-        <?php foreach ($visited_specialities as $speciality) : ?>
+        <?php foreach ($specialities as $speciality) : ?>
             <option value="<?= $speciality->post_name ?>"><?= $speciality->post_title ?></option>
         <?php endforeach; ?>
     </select>
